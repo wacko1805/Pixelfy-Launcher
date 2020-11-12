8380f545027dd0c5b6f0f48b3f7db7e11cb2b218
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2020-11-12 19:51:09 +1100

    Updated FIles

--- customize.sh
+++ customize.sh
@@ -9,6 +9,6 @@
 ##########################################################################################
 
 #Print welcome message
-ui_print "- Installing Pixel-fy launcher V0.2 BETA"
+ui_print "- Installing Pixel-fy launcher V0.4 BETA"
 ui_print "- By Wacko, reddit: wacko1805"
 ui_print "- Visit my site for more: pixel-fy.com"
